638684b61698d2573164288a6a9d8e1d551bec24
ataerg <ataerg@github.com>
2022-03-03 01:13:59 +0100

    Authentification sistem and all products working

--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,24 @@ use App\Http\Controllers\OrderController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-/*
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
 Route::post('register', [PassportAuthController::class, 'register']);
 Route::post('login', [PassportAuthController::class, 'login']);
-Route::resource('products', ProductController::class);
+//Route::resource('products', ProductController::class);
+Route::controller(ProductController::class)->group(function () {
+    Route::get('products', 'index');
+    Route::get('products/{product}', 'show');
+});
+
+Route::middleware('auth:api','role')->group(function () {
+    Route::middleware(['scope:admin'])->group(function () {
+        Route::controller(ProductController::class)->group(function () {
+            Route::post('products', 'create');
+            Route::put('products', 'update');
+            Route::delete('products', 'destroy');
+        });
+});
+});
+
 Route::middleware('auth:api')->group(function () {
     Route::resource('comments', CommentController::class);
     Route::resource('orders', OrderController::class);
